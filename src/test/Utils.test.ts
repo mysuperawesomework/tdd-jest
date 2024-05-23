903ecd5e686479db3d866f1a511624aa63277de1
mysuperawesomework <my@superawesome.work>
2024-05-22 22:23:30 -0400

    Adds a custom type for testing

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,8 @@
-import { toUpperCase } from "../app/Utils";
+import { getStringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', ()=> {
 
-    test('should return uppercase of valid string', ()=>{
+    it('should return uppercase of valid string', ()=>{
         // Arrange:
         const sut = toUpperCase; 
         const expected = 'ABC'
@@ -12,6 +12,21 @@ describe('Utils test suite', ()=> {
 
         // Assert:
         expect(actual).toBe('ABC');
+    });
+
+    it.only('shold return info for valid string', ()=>{
+        const actual = getStringInfo('My-String');
+
+        expect(actual.lowerCase).toBe('my-string');
+        expect(actual.extraInfo).toEqual({});
+
+        expect(actual.characters).toHaveLength(9);
+
+        expect(actual.characters).toContain<string>('M');
+        expect(actual.characters).toEqual(
+            expect.arrayContaining(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g'])
+        );
+        expect(actual.extraInfo).toBeTruthy();
     });
 
 });

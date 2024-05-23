9212d514c4cbb95fcbc31db54e4c6324d7705f5f
mysuperawesomework <my@superawesome.work>
2024-05-22 22:02:52 -0400

    Updated with testing AAA principles

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # TDD-JEST
 A repo to learn tdd
+
+
+All tests should subscribe to the AAA principles
+
+### AAA principles:
+- Arrange
+- Act
+- Assert
+
+### Terminology
+
+SUT - Abbreviation for System Under Test
+
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,9 +2,16 @@ import { toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', ()=> {
 
-    test('Should return uppercase', ()=>{
-        const result = toUpperCase('abc');
-        expect(result).toBe('ABC');
+    test('should return uppercase of valid string', ()=>{
+        // Arrange:
+        const sut = toUpperCase; 
+        const expected = 'ABC'
+
+        // Act:
+        const actual = toUpperCase('abc');
+
+        // Assert:
+        expect(actual).toBe('ABC');
     });
 
 });

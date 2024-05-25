5b94edd3051d734e972f812201c594efe8f769d2
mysuperawesomework <my@superawesome.work>
2024-05-24 21:29:51 -0400

    Configure code coverage report

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,7 +2,7 @@ import { StringUtils, getStringInfo, toUpperCase } from "../app/Utils";
 
 describe('Utils test suite', ()=> {
 
-    describe.only('StringUtils tests', ()=>{
+    describe('StringUtils tests', ()=>{
 
             let sut: StringUtils;
 

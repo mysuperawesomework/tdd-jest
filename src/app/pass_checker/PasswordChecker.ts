ee779b561a5d25898943cd69ffa43d96c0c4e1fd
mysuperawesomework <my@superawesome.work>
2024-05-26 08:41:53 -0400

    Refactors password checks to private methods

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -15,21 +15,31 @@ export class PasswordChecker {
     public checkpassword(password: string): CheckResult {
         const reasons:PasswordErrors[] = [];
 
-        if(password.length < 8) {
-            reasons.push(PasswordErrors.SHORT);
-        }
-
-        if(password === password.toLowerCase()) {
-            reasons.push(PasswordErrors.NO_UPPER_CASE);
-        }
-
-        if(password === password.toUpperCase()) {
-            reasons.push(PasswordErrors.NO_LOWER_CASE);
-        }
+        this.checkForLength(password, reasons);
+        this.checkForUpperCase(password, reasons);
+        this.checkForLowerCase(password, reasons);
         
         return {
             valid: reasons.length > 0 ? false : true,
             reasons: reasons
+        }
+    }
+
+    private checkForLength(password: string, reasons: PasswordErrors[]) {
+        if(password.length < 8) {
+            reasons.push(PasswordErrors.SHORT);
+        }
+    }
+
+    private checkForUpperCase(password: string, reasons: PasswordErrors[]) {
+        if(password === password.toLowerCase()) {
+            reasons.push(PasswordErrors.NO_UPPER_CASE);
+        }
+    }
+
+    private checkForLowerCase(password: string, reasons: PasswordErrors[]) {
+        if(password === password.toUpperCase()) {
+            reasons.push(PasswordErrors.NO_LOWER_CASE);
         }
     }
 
